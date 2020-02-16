2d78fb4b6fa396dd80ef530617223b70b9afedc4
허정근 <gjek136@naver.com>
2020-02-16 22:13:22 +0900

    [기능] firebase 연동 파일 수정
    1. 기존 프로젝트로 string 이 설정되있던 것을 현재 프로젝트로 변경

--- app/src/main/java/com/jucanos/photomap/Activity/LoginActivity.java
+++ app/src/main/java/com/jucanos/photomap/Activity/LoginActivity.java
@@ -14,13 +14,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.jucanos.photomap.GlobalApplication;
