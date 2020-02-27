2af1301b99bee8bd86abdf34ec88766361a30f3e
허정근 <gjek136@naver.com>
2020-02-28 03:48:30 +0900

    [기능] loading progress dialog로 대체

--- app/src/main/java/com/jucanos/photomap/Activity/AddStoryPeedActivity.java
+++ app/src/main/java/com/jucanos/photomap/Activity/AddStoryPeedActivity.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.jucanos.photomap.Dialog.LoadingDialog;
 import com.jucanos.photomap.GlobalApplication;
 import com.jucanos.photomap.R;
 import com.jucanos.photomap.RestApi.NetworkHelper;
@@ -49,9 +50,8 @@ public class AddStoryPeedActivity extends AppCompatActivity implements View.OnCl
     //  private ViewPager viewPager;
 
 
-    private CardView box;
-    private String LOADING_PROGRESS = "loading_progress";
     private InputMethodManager mKeyBord;
+    private LoadingDialog loadingDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,10 +61,7 @@ public class AddStoryPeedActivity extends AppCompatActivity implements View.OnCl
         getIntentData();
         initMember();
         setToolbar();
-//        setLayoutSize();
-//        loadImages();
         setBox();
-
         mKeyBord.showSoftInput(editText_title, 0);
     }
 
@@ -78,15 +75,10 @@ public class AddStoryPeedActivity extends AppCompatActivity implements View.OnCl
     }
 
     public void initMember() {
-//        viewPager = findViewById(R.id.viewPager);
-//        textView_indicator= findViewById(R.id.tv_indicator);
-        // textView_upload = findViewById(R.id.textView_upload);
         editText_title = findViewById(R.id.editText_title);
         editText_context = findViewById(R.id.editText_context);
-
         rv_total = findViewById(R.id.rl_total);
         mKeyBord = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        //textView_upload.setOnClickListener(this);
     }
 
     public void setToolbar() {
@@ -96,51 +88,14 @@ public class AddStoryPeedActivity extends AppCompatActivity implements View.OnCl
         textView_cancel.setOnClickListener(this);
     }
 
-//    private void setLayoutSize() {
-//        viewPager.getLayoutParams().height = ViewUtils.getScreenWidth();
-//        viewPager.getLayoutParams().width = ViewUtils.getScreenWidth();
-//    }
-
-//    public void loadImages() {
-//        addStoryImageSliderAdapter = new AddStoryImageSliderAdapter(this, paths);
-//        viewPager.setAdapter(addStoryImageSliderAdapter);
-//        String indicator =  1 + "/" + paths.size();
-//        textView_indicator.setText(indicator);
-//        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) {
-//                String indicator = (position + 1) + "/" + paths.size();
-//                textView_indicator.setText(indicator);
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//
-//            }
-//        });
-//    }
-
     @SuppressLint("ClickableViewAccessibility")
     private void setBox() {
-        box = findViewById(R.id.box);
-        box.setVisibility(View.GONE);
-        box.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return true;
-            }
-        });
+        loadingDialog = new LoadingDialog(this);
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
-
         switch (id) {
             case R.id.textView_upload:
                 String title = editText_title.getText().toString();
@@ -164,9 +119,9 @@ public class AddStoryPeedActivity extends AppCompatActivity implements View.OnCl
     }
 
     public void requestUploadImage(String title, String context, ArrayList<String> paths) {
+        loadingDialog.show();
         mKeyBord.hideSoftInputFromWindow(editText_context.getWindowToken(), 0);
         mKeyBord.hideSoftInputFromWindow(editText_title.getWindowToken(), 0);
-        box.setVisibility(View.VISIBLE);
         RequestBody requetCityKey = RequestBody.create(MediaType.parse("text/plain"), cityKey);
         RequestBody requestTitle = RequestBody.create(MediaType.parse("text/plain"), title);
         RequestBody requestDescription = RequestBody.create(MediaType.parse("text/plain"), context);
@@ -190,11 +145,11 @@ public class AddStoryPeedActivity extends AppCompatActivity implements View.OnCl
                 if (response.isSuccessful()) {
                     Log.e("AddStoryPeedActivity", "response.isSuccessful()");
                     if (response.body() != null) {
-//                        Log.e("AddStoryActivity", "[mid] : " + response.body().getCreateStoryData().getMid());
-//                        Log.e("AddStoryActivity", "[title] : " + response.body().getCreateStoryData().getTitle());
-//                        Log.e("AddStoryActivity", "[content] : " + response.body().getCreateStoryData().getContext());
-//                        Log.e("AddStoryActivity", "[files] : " + response.body().getCreateStoryData().getFiles().toString());
-//                        Log.e("AddStoryActivity", "[sid] : " + response.body().getCreateStoryData().getSid());
+                        Log.i("AddStoryActivity", "[mid] : " + response.body().getCreateStoryData().getMid());
+                        Log.i("AddStoryActivity", "[title] : " + response.body().getCreateStoryData().getTitle());
+                        Log.i("AddStoryActivity", "[content] : " + response.body().getCreateStoryData().getContext());
+                        Log.i("AddStoryActivity", "[files] : " + response.body().getCreateStoryData().getFiles().toString());
+                        Log.i("AddStoryActivity", "[sid] : " + response.body().getCreateStoryData().getSid());
                         Intent intent = new Intent();
                         intent.putExtra("title", response.body().getCreateStoryData().getTitle());
                         intent.putExtra("context", response.body().getCreateStoryData().getContext());
@@ -202,18 +157,21 @@ public class AddStoryPeedActivity extends AppCompatActivity implements View.OnCl
                         intent.putExtra("mid", response.body().getCreateStoryData().getMid());
                         intent.putExtra("sid", response.body().getCreateStoryData().getSid());
                         setResult(RESULT_OK, intent);
-                        box.setVisibility(View.GONE);
+                        loadingDialog.dismiss();
                         finish();
                     }
                 } else {
                     Log.e("AddStoryPeedActivity", "response.isNotSuccessful()");
-
+                    Toast.makeText(AddStoryPeedActivity.this, "업로드 실패", Toast.LENGTH_SHORT).show();
+                    loadingDialog.dismiss();
                 }
             }
 
             @Override
             public void onFailure(Call<CreateStory> call, Throwable t) {
                 Log.e("AddStoryPeedActivity", "onFailure : " + t.getLocalizedMessage());
+                Toast.makeText(AddStoryPeedActivity.this, "업로드 실패", Toast.LENGTH_SHORT).show();
+                loadingDialog.dismiss();
             }
         });
     }
