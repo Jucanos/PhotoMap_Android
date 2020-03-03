86143fd76f96db6c86365ee5f4e5bd6465db4a93
heojeonggeun <gjek136@naver.com>
2020-03-03 18:44:36 +0900

    [기능] dev url로 변경

--- app/src/main/java/com/jucanos/photomap/RestApi/NetworkHelper.java
+++ app/src/main/java/com/jucanos/photomap/RestApi/NetworkHelper.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NetworkHelper {
-    private static String url = "https://soybeans.tech/api/";
+    private static String url = "https://soybeans.tech/dev/";
 
 
 
